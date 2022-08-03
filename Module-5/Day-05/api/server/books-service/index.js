8856a3a56a8676d8fae0411ebbc0a368ea5266b3
Brayden Robbins <braydenrobbins98@gmail.com>
2022-08-03 17:17:37 -0500

    Adding Day 3 to Day 5 for starting point

---
+++ Module-5/Day-05/api/server/books-service/index.js
@@ -0,0 +1,33 @@
+import dbBooks from "./books.json";
+
+let books = [...dbBooks];
+
+const getAll = () => {
+  return books;
+};
+
+const add = (newBook) => {
+  const newBooks = [...books, newBook];
+  books = newBooks;
+
+  return newBook;
+};
+
+const updateById = (id, updates) => {
+  const bookIndex = books.findIndex((book) => book.id === id);
+  const updatedBook = { ...books[bookIndex], ...updates };
+  books[bookIndex] = updatedBook;
+
+  return updatedBook;
+};
+
+const deleteById = (id) => {
+  const filteredBooks = books.filter((book) => book.id !== id);
+  books = filteredBooks;
+
+  return books;
+};
+
+const BooksService = { getAll, add, updateById, deleteById };
+
+export default BooksService;
