9511c8a3f5fba6c08e9d493ce18c5ff1b5184fc4
Zach Mays <zsmays@gmail.com>
2023-07-22 12:05:22 -0500

    add default data

--- Module-5/Day-02/our-first-next-app/src/coffeeDb.js
+++ Module-5/Day-02/our-first-next-app/src/coffeeDb.js
@@ -1,4 +1,7 @@
-const menu = [];
+const menu = [
+  { id: 1, name: "pour over", price: 3.5, description: "single origin" },
+  { id: 2, name: "cold brew", price: 4.5, description: "single origin" },
+];
 
 export function addMenuItem(name, price, description) {
   const item = {
