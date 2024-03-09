a8e74eed7c586a71d795caccf78acb1198927ebb
Erich Keil <erich.keil@clevyr.com>
2024-03-09 12:09:17 -0600

    docs: m2d2 post class update

--- Module-2/Day-2/DOMexample.js
+++ Module-2/Day-2/DOMexample.js
@@ -1,42 +1,60 @@
-const dom = {
-	tagName: 'div',
-	attributes: {
-		id: 'container',
-		class: 'main-container',
-	},
-	children: [
-		{
-			tagName: 'h1',
-			attributes: {
-				class: 'title',
-			},
-			children: ['Hello, World!'],
-		},
-		{
-			tagName: 'p',
-			attributes: {},
-			children: ['This is a sample paragraph.'],
-		},
-		{
-			tagName: 'ul',
-			attributes: {},
-			children: [
-				{
-					tagName: 'li',
-					attributes: {},
-					children: ['Item 1'],
-				},
-				{
-					tagName: 'li',
-					attributes: {},
-					children: ['Item 2'],
-				},
-				{
-					tagName: 'li',
-					attributes: {},
-					children: ['Item 3'],
-				},
-			],
-		},
-	],
+const document = {
+    tagName: 'html',
+    attributes: {
+        lang: 'en'
+    },
+    children: [
+        {
+            tagName: 'head'
+        },  
+        {
+            tagName: 'body',
+            attributes: {},
+            children: [
+                {
+                    tagName: 'div',
+                    attributes: {
+                        id: 'container',
+                        class: 'main-container',
+                    },
+                    children: [
+                        {
+                            tagName: 'h1',
+                            attributes: {
+                                class: 'title',
+                            },
+                            children: ['Hello, World!'],
+                        },
+                        {
+                            tagName: 'p',
+                            attributes: {},
+                            children: ['This is a sample paragraph.'],
+                        },
+                        {
+                            tagName: 'ul',
+                            attributes: {},
+                            children: [
+                                {
+                                    tagName: 'li',
+                                    attributes: {},
+                                    children: ['Item 1'],
+                                },
+                                {
+                                    tagName: 'li',
+                                    attributes: {},
+                                    children: ['Item 2'],
+                                },
+                                {
+                                    tagName: 'li',
+                                    attributes: {},
+                                    children: ['Item 3'],
+                                },
+                            ],
+                        },
+                    ],
+                }
+            ]
+
+        }
+    ]
 };
--- Module-2/Day-2/dialog.js
+++ Module-2/Day-2/dialog.js
@@ -1,5 +1,26 @@
 // get info from user with prompt - ask for name
+const userName = prompt("Please enter first and last name");
 // add greeting message to the document with html
+const titleDisplay = document.getElementById('greeting');
+titleDisplay.innerHTML = `<p>Hello, ${userName[0]}</p>`;
 // add button to randomly change background color
-// - string concatenation/indexing
-// - user avatar first name initial and last name initial from name string
+const colorButton = document.getElementById('color-changer')
+let colorIndex = 0;
+colorButton.onclick = changeColor
+
+// string concatenation/indexing/template literals
+// user avatar first name initial and last name initial from name string
+
+function changeColor() {
+    //do the thing
+    const body = document.querySelector('body');
+    const colors = ["red", "green", "blue"];
+    console.log("setting color to index: ", colorIndex);
+    body.style.backgroundColor = colors[colorIndex]
+    
+    if (colorIndex < colors.length - 1) {
+        colorIndex++
+    } else {
+        colorIndex = 0;
+    }
+}
