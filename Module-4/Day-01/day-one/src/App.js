6c912b014f38a6bae2ecadf44ba015d315df595b
Bryan Rigsby <bryanrigsby@att.net>
2024-05-15 07:45:41 -0500

    added Module-4 day-1 to current curriculum

---
+++ Module-4/Day-01/day-one/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Body from './components/Body';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Body /> 
+      <Footer />
+    </>
+  );
+}
+
+export default App;
