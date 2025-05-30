58ffa7c82c43575980507f8c0eaadfded6330719
Derrick D. Young <youngderrick82@gmail.com>
2025-05-30 00:13:44 -0500

    added day 3 folder and file

---
+++ 2025/Module-4/_Overview/src/day3/day3.jsx
@@ -0,0 +1,29 @@
+
+
+
+
+
+export default function Day3 () {
+
+    let Component = ""
+    switch (selectedTab) {
+        case "Fetch API":
+            Component = <FetchApi />
+
+    }
+
+
+
+    return (
+        <div>
+            <h1>Module 4: Day 3</h1>
+            <div className="lesson-tabs">
+
+            </div>
+            <div className="lesson-content">
+                {}
+
+            </div>
+        </div>
+    )
+}
